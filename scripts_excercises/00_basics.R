d858dcd8e8324256298be13aed39ab152aec0cc3
pastoetter <pastoetter@uni-trier.de>
2021-05-31 11:33:38 +0200

    weiter

--- scripts_excercises/00_basics.R
+++ scripts_excercises/00_basics.R
@@ -7,21 +7,36 @@
 
 # 1.1) Type a number into the console. What happens?
 
+2
+
 # 1.2) Try a decimal.
+
+2.3
 
 # 1.3) Type some mathematical statement into the console, e.g. 3 + 5 or 2 - 10. What happens?
 
+3+5
+2-10
+
 # 1.4) How do you multiply numbers? You do you divide them?
+
+4*4
+4/4
 
 # 1.5) What does : do? (e.g. 1:4)
 
+1:4
+
 # 1.6) Try typing a word (e.g. test). What happens?
+
+"test"
 
 # Working in the console is a bit messy, because you can't save it anywhere. However, we can also
 # code inside this script and then run it - the output will appear in the console.
 # We don't need to run the whole script every time - we can also run individual lines by
 # pressing ctrl + return.
 # Try to run the following line using ctrl + return:
+
 41 + 1
 
 #### 2 DATA TYPES ####
@@ -36,6 +51,7 @@
 
 # You can find out what data type something is by using the function typeof(). It works
 # like this:
+
 typeof(3.2)
 
 # This should return "double". Now try it yourself - type the answers below the questions
@@ -43,11 +59,19 @@ typeof(3.2)
 
 # 2.1) What data type is 2? Think first, then try typeof(). Did you expect this result?
 
+typeof(2)
+
 # 2.2) What data type is 1:4?
+
+typeof(1:4)
 
 # 2.3) What data type is "R"? What is "I love R"?
 
+typeof("R")
+
 # 2.4) What data type is TRUE? What data type is FALSE? (Don't use quotation marks around these!)
+
+typeof(TRUE)
 
 #### 3 LOGICALS ####
 
@@ -56,22 +80,45 @@ typeof(3.2)
 
 # 3.1) Type 12 > 3 into your console. Then, type 12 < 3.
 
+12 > 3
+12 < 3
+
 # 3.1) Try 12 == 12. And now 12 == 13. What does == do?
+
+12 == 12
+12 == 13
 
 # 3.2) Try out: FALSE & FALSE, then FALSE & TRUE and finally, TRUE & TRUE. What happens?
 
+FALSE & FALSE
+TRUE & TRUE
+FALSE & TRUE
+
 # 3.3) Try the same as above, but replace & with | (alt gr + <). Do you know what do & and | do?
+
+FALSE | FALSE
+TRUE | TRUE
+FALSE | TRUE
 
 # 3.4) What do you think will be the result of 4 < 3 | 4 == 4? What is 4 < 3 & 4 == 4?
 
+4 < 3 | 4 == 4
+4 < 3 & 4 == 4
+
 # 3.5) What is "cat" != "dog"?
 
+"cat" != "dog"
+
 # 3.6) What is "cat" > "dog"? What about other words? Can you find a pattern?
+
+"cat" > "dog"
 
 # 3.7) By the way, did you know that logicals have numeric representations? Try TRUE + 1.
 #      Then try FALSE + 1. What is the numeric value of TRUE? What is the numeric value of FALSE?
 
-#### 4 VARIABLES ####
+TRUE + 1
+
+read#### 4 VARIABLES ####
 
 # This is all fun and games, but we actually want to save things an keep them for longer. 
 # For this purpose, we use variables. Variables are like little containers that can hold
@@ -81,14 +128,20 @@ typeof(3.2)
 
 single_number <- 7
 
+# <- (alt -)
+
+  
 # ALWAYS USE VARIABLE NAMES THAT TELL YOU WHAT THAT VARIABLE CONTAINS!!!
 
 # 4.1) Type single_number into the console and hit return.
+
+2
 
 # 4.2) Run the following code. What will happen now if you type single_number into the console?
 single_number <- 2
 
 # 4.3) Assign something to a variable with a name of your choice.
+other_number <- 10
 
 # 4.4) Examine what happens here. What is the result? What do you think will single_number
 #      contain now? 2 or 6?
